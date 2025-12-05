b84d261aab131e0523a2ac67163b5f3eb0a9866a
Florian Cossu <florian.cossu@bodyguard.ai>
2025-12-05 22:34:44 +0100

    Started work pn day 4 part II Also updated scripts because they are both loaded in the head and their variables and consts can conflict

--- Day_04/js/part2.js
+++ Day_04/js/part2.js
@@ -1,25 +1,29 @@
 function part2Main(input) {
     let lines = input.split('\n');
     let diagram = [];
+    let tmpDiagram = []
     let count = 0;
     
     for (let line of lines) {
         diagram.push(line.trim().split(""))
+        tmpDiagram.push(line.trim().split(""))
     }
     
     // console.log(diagram)
 
-    const diagY = diagram.length - 1
-    const diagX = diagram[0].length - 1
+    const diagY2 = diagram.length - 1
+    const diagX2 = diagram[0].length - 1
 
-    // console.log({diagX, diagY})
+    // console.log({diagX2, diagY2})
 
-    for (let i = 0; i < diagY+1; i++) {
-        for (let j = 0; j < diagX+1; j++) {
-            parseInt(checkAdjacent(diagram, i, j)) == 1 ? count++ : null;
-            // console.log(`Adding ${checkAdjacent(diagram, i, j)}`)
+    for (let i = 0; i < diagY2+1; i++) {
+        for (let j = 0; j < diagX2+1; j++) {
+            parseInt( checkAdjacent2(diagram, tmpDiagram, i, j)) == 1 ? count++ : null;
+            // console.log(`Adding ${ checkAdjacent2(diagram, i, j)}`)
         }
     }
+
+    console.log(tmpDiagram)
 
     resultDisplay(count);
 }
@@ -31,8 +35,9 @@ function part2Main(input) {
  * @param {int} col The column of the line from the diagram
  * @returns {int} Returns 1 or 0 depending on whether the current index in the diagram satisfies the no more than 4 @ in the adjacent positions
  */
-function checkAdjacent (diagram, line, col) {
+function  checkAdjacent2 (diagram, tmpDiagram, line, col) {
     let adjCount = 0
+    let result = 0;
     let posContents = diagram[line][col]
 
     diagram[line-1] !== undefined && diagram[line-1][col-1] !== undefined && posContents === "@" && diagram[line-1][col-1] === "@" ? adjCount++ : null;
@@ -46,5 +51,10 @@ function checkAdjacent (diagram, line, col) {
 
     // console.log(`Line ${line}, col ${col}, posContents ${posContents}, adjCount ${adjCount}, ${adjCount < 4 && posContents === "@" ? "adding to roll count" : "not adding to roll count"}`)
 
-    return adjCount < 4 && posContents === "@" ? 1 : 0;
+    if (adjCount < 4 && posContents === "@") {
+        tmpDiagram[line][col] = "X"
+        result = 1
+    }
+
+    return result
 }
